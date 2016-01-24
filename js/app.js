b88bb72034fa6d8acecd9780ea182157519e7f3b
Oliver Banham <oliverbanham@Gmail.com>
2016-01-24 19:32:01 +0000

    Split more areas into their own functions

--- js/app.js
+++ js/app.js
@@ -1,30 +1,42 @@
 var toDoListApp = {
 
 	// VARIABLES
-	jsButtons: "<a class='small-btn btn pull-right js-delete delete-btn' href='#'>Delete</a> <a class='small-btn btn pull-right js-mark-as-complete' href='#'>Mark as Complete</a>",
+	jsBtn: "<a class='small-btn btn pull-right js-delete delete-btn' href='#'>Delete</a> <a class='small-btn btn pull-right js-mark-as-complete' href='#'>Mark as Complete</a>",
 	allGood : true,
 	completedItems: [],
 	totalItemsCount: 0,
 	completedItemsCount: 0,
 
 	init: function(){
-		this.cacheSelectors();
-		this.bindEvents();
+		this.cacheSelectors(true);
+		this.bindEvents(true);
 	},
 
-	cacheSelectors: function () {
-		this.toDoList = document.querySelector(".js-to-do-list");
-		this.addBtn = this.toDoList.querySelector(".js-add");
-		this.myToDos = this.toDoList.querySelector(".js-my-todos");
-		this.mainInput = this.toDoList.querySelector(".js-main-input");
-		this.totalItemsSpan = this.toDoList.querySelector(".js-total-items");
-		this.completedItemsSpan = this.toDoList.querySelector(".js-completed-items");
-		this.completedItemsList = this.toDoList.querySelector(".js-my-completed-items");
+	cacheSelectors: function (onInit) {
+		if(onInit) {
+			this.toDoList = document.querySelector(".js-to-do-list");
+			this.addBtn = this.toDoList.querySelector(".js-add");
+			this.myToDos = this.toDoList.querySelector(".js-my-todos");
+			this.mainInput = this.toDoList.querySelector(".js-main-input");
+			this.totalItemsSpan = this.toDoList.querySelector(".js-total-items");
+			this.completedItemsSpan = this.toDoList.querySelector(".js-completed-items");
+			this.completedItemsList = this.toDoList.querySelector(".js-my-completed-items");
+		} else {
+			// elements not available until created
+			this.markAsCompleteBtn = this.toDoList.querySelector(".js-mark-as-complete");
+			this.deleteBtn = this.toDoList.querySelector(".js-delete");
+		}
 	},
 
-	bindEvents: function(){
-		this.addBtn.addEventListener("click", this.addToDoItem.bind(this));
-		this.mainInput.addEventListener("keydown", this.checkEnter.bind(this));
+	bindEvents: function(onInit){
+		if(onInit) {
+			this.addBtn.addEventListener("click", this.addToDoItem.bind(this));
+			this.mainInput.addEventListener("keydown", this.checkEnter.bind(this));
+		} else{
+			// elements not available until created
+			this.markAsCompleteBtn.addEventListener("click", this.markAsComplete.bind(this));
+			this.deleteBtn.addEventListener("click", this.deleteItem.bind(this));
+		}
 	},
 
 	checkEnter: function(e){
@@ -49,55 +61,67 @@ var toDoListApp = {
 	},
 
 	addToDoItem: function(e){
-		// Prevent button's default behaviour
+
 		e.preventDefault();
+
 		this.validation(this.mainInput);
-		// if validation function has set allGood variable to false, return out the function
-		if(this.allGood === false){
+
+		// if validation function has set allGood variable to false, return out of the function
+		if(!this.allGood){
 			return false;
 		}
 
-		this.addAndShowTotalItems();
-		// Create new Li element each time button is clicked, add buttons to li
-		var li = document.createElement("li");
-		li.innerHTML = this.jsButtons + "<span class='js-value'>" + this.mainInput.value + "</span>";
-
-		// Prepend (insert before the first child of the ul, which will always be the latest li element) new Li element to UL, then fill it with the value of the input field
-		this.myToDos.insertBefore(li, this.myToDos.firstChild);
-		// Reset input
-		this.mainInput.value = "";
-
-		// Select mark as complete button and delete buttons
-		var markAsCompleteBtn = this.toDoList.querySelector(".js-mark-as-complete");
-		var deleteBtn = this.toDoList.querySelector(".js-delete");
-
-		// Add event listener to button
-		markAsCompleteBtn.addEventListener("click", this.markAsComplete.bind(this));
-		deleteBtn.addEventListener("click", this.deleteItem.bind(this));
+		// build item
+		this.buildToDoItem();
+		// render to page
+		this.renderToDoItem();
+		// increment total items counter
+		this.incrementTotalItems();
+		// select new button elements and cache them
+		this.cacheSelectors(false);
+		// bind new listeners to new elements
+		this.bindEvents(false);
 
 	},
 
-	addAndShowTotalItems: function(){
+	buildToDoItem: function(){
+		// Create new Li element each time button is clicked, add buttons to li
+		this.li = document.createElement("li");
+		this.li.innerHTML = this.jsBtn + "<span class='js-value'>" + this.mainInput.value + "</span>";
+	},
+
+	renderToDoItem: function(){
+		// Prepend (insert before the first child of the ul, which will always be the latest li element) new Li element to UL, then fill it with the value of the input field
+		this.myToDos.insertBefore(this.li, this.myToDos.firstChild);
+		// Reset input
+		this.mainInput.value = "";
+	},
+
+	incrementTotalItems: function(){
 		// function that increments total item counter and prints to page
 		this.totalItemsCount++;
 		this.totalItemsSpan.innerHTML = this.totalItemsCount;
 	},
 
+	incrementCompletedItems: function(){
+		// increment completed item count and print to page
+		this.completedItemsCount++;
+		this.completedItemsSpan.innerHTML = this.completedItemsCount;
+	},
+
 	deleteItem: function(e){
-		// remove item
 		e.target.parentNode.remove();
 	},
 
 	markAsComplete: function(e){
-		// increment completed item count and print to page
-		this.completedItemsCount++;
-		this.completedItemsSpan.innerHTML = this.completedItemsCount;
+		this.incrementCompletedItems();
 		// push the 'text' of the completed item to an array
 		this.completedItems.push(e.target.parentNode.querySelector(".js-value").innerHTML);
-		// add completed css class name to  to-do-list item
+		// add completed css class name to to-do-list item
 		e.target.parentNode.className = "completed";
 		// add text from completed items array to list. Slice method is used so the last item in the array is grabbed and returned
 		this.completedItemsList.innerHTML += "<li>" + this.completedItems.slice(-1)[0] + "</li>";
+		e.target.remove();
 	}
 
 };
